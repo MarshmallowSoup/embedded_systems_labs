5ad4e2dcbc8d852d81660ee7631fb2a271c309ed
Volodymyr Lysetskyi <vlysetskyi@dedicatted.com>
2023-10-13 01:07:22 +0300

    refactored lab1 and lab2

--- lab1/main.c
+++ lab1/main.c
@@ -2,47 +2,53 @@
 #include <util/delay.h>
 #define LED_PIN PB5 // Built-in LED on Arduino Nano
 
-void flashDot() {
-    PORTB |= 1 << LED_PIN; // Turn on the LED (dot)
-    _delay_ms(200); // Dot duration
-    PORTB &= ~(1 << LED_PIN); // Turn off the LED
-    _delay_ms(200); // Gap between dots and dashes
+void flashDot()
+{
+    PORTB |= 1 << LED_PIN; 
+    _delay_ms(200); 
+    PORTB &= ~(1 << LED_PIN); 
+    _delay_ms(200);
 }
 
-void flashDash() {
-    PORTB |= 1 << LED_PIN; // Turn on the LED (dash)
-    _delay_ms(600); // Dash duration
-    PORTB &= ~(1 << LED_PIN); // Turn off the LED
-    _delay_ms(200); // Gap between dots and dashes
+void flashDash() 
+{
+    PORTB |= 1 << LED_PIN; 
+    _delay_ms(600); 
+    PORTB &= ~(1 << LED_PIN); 
+    _delay_ms(200); 
+}
+
+void blink_msg(char *msg)
+{
+    while(*msg) {
+        // ----
+        if(*msg == "-"){
+            flashDash();
+        }
+        else if(*msg == "."){
+            flashDot();
+        }
+        else if(*msg == " "){
+            _delay_ms(400);
+        }
+        else{
+            break;
+        }
+        msg++;
+    }
+
+    _delay_ms(2000);
 }
 
 int main(void) {
     DDRB |= 1 << LED_PIN;
 
-    while (1) {
-        // "S" in Morse code: dot-dot-dot
-        for (int i = 0; i < 3; i++) {
-            flashDot();
-        }
-        _delay_ms(400); // Gap between letters
 
-        // "O" in Morse code: dash-dash-dash
-        for (int i = 0; i < 3; i++) {
-            flashDash();
-        }
-        _delay_ms(400); // Gap between letters
-
-        // "S" in Morse code: dot-dot-dot
-        for (int i = 0; i < 3; i++) {
-            flashDot();
-        }
-        
-        // Delay before shutting down the LED
-        _delay_ms(2000);
+    blink_msg("... --- ...");
 
         // Turn off the LED
-        PORTB &= ~(1 << LED_PIN);
-    }
+    PORTB &= ~(1 << LED_PIN);
+    
 
     return 0;
 }
