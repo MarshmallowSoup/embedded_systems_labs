6794b3cef9030fe3d6e5aa85a1b1240a5adb15a9
vlysetskyi <vlysetskyi@dedicatted.com>
2023-10-15 15:03:19 +0300

    Updated lab2

--- lab2/main.c
+++ lab2/main.c
@@ -1,93 +1,83 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define NUM_LEDS 6  // Number of LEDs
-#define DELAY_MS 100 // Delay between LED transitions
-#define BUTTON_PIN PD2 // Pin for the button
-#define LED_PORT PORTD
+uint8_t direction = 0, data = 1, is_run = 1;
+void key_handler(uint8_t ks);
 
-int ledPins[NUM_LEDS] = {PD7, PD6, PD5, PD4, PB0, PB1};  // Pins for the LEDs
-int currentEffect = 0;  // Current selected effect
+void scan_key(void)
+{
+    static uint8_t shreg = 0xFF;
+    static uint8_t long_press_cnt = 0;
 
-void initializeLEDs() {
-    for (int i = 0; i < NUM_LEDS; i++) {
-        // Set the LED pins as outputs
-        DDRD |= (1 << ledPins[i]);
+    shreg <<= 1;
+    if((PINB & (1 << PB1)) != 0) {
+        shreg |= 1;
+    }
+    // push key
+    if((shreg & 0x07) == 0x04) {
+        long_press_cnt = 0;
+        key_handler(0);
+    }
+    // up key
+    if((shreg & 0x0F) == 0x03) {
+        if(long_press_cnt < 100){
+        key_handler(1);
+        }
+    }
+    if(shreg == 0){
+        if(long_press_cnt < 254){
+            long_press_cnt++;
+        }
+        if (long_press_cnt == 100) {
+            key_handler(2); //long press
+        } 
+    }
+}
+//-----------------------------------------------------------
+void key_handler(uint8_t ks)
+{
+    if (ks == 1) { // short key up
+        is_run = (is_run == 0)? 1: 0;
+        
+    } else if (ks == 2) {  //long key press
+        direction ^=1;
+        is_run = 1;
     }
 }
 
-void runningLight() {
-    for (int i = 0; i < NUM_LEDS; i++) {
-        // Turn on the i-th LED
-        PORTD |= (1 << ledPins[i]);
-        _delay_ms(DELAY_MS);
-        // Turn off the i-th LED
-        PORTD &= ~(1 << ledPins[i]);
-    }
+//-----------------------------------------------------------
+int main(void)
+{ 
+    uint8_t dl = 0;
+    // Налаштування портів вводу-виводу
+    DDRD = 0xFF; // Всі лінії порта D виходи
+    DDRB = 0; // Всі лінії порта B входи
+    UCSR0B = 0;
 
-    for (int i = NUM_LEDS - 1; i >= 0; i--) {
-        // Turn on the i-th LED
-        PORTD |= (1 << ledPins[i]);
-        _delay_ms(DELAY_MS);
-        // Turn off the i-th LED
-        PORTD &= ~(1 << ledPins[i]);
-    }
-}
+    for (;;) {
+        // ~200 ms thread -----
+        if (is_run) {
+            if (++dl > 5) {
+                dl = 0;
 
-void shadow() {
-    for (int i = 0; i < NUM_LEDS; i++) {
-        // Turn on the i-th LED
-        PORTD |= (1 << ledPins[i]);
-        _delay_ms(DELAY_MS);
-        // Turn off the i-th LED
-        PORTD &= ~(1 << ledPins[i]);
-    }
-
-    for (int i = NUM_LEDS - 1; i >= 0; i--) {
-        // Turn on the i-th LED
-        PORTD |= (1 << ledPins[i]);
-        _delay_ms(DELAY_MS);
-        // Turn off the i-th LED
-        PORTD &= ~(1 << ledPins[i]);
-    }
-}
-
-void changeEffect() {
-    currentEffect = (currentEffect + 1) % 2;  // Toggle between effects (assuming 2 effects for simplicity)
-}
-
-int main(void) {
-    // Set the LED pins and button pin as outputs and inputs
-    DDRD &= ~(1 << BUTTON_PIN);  // Set the button pin as an input
-    initializeLEDs();  // Initialize LED pins
-
-    int buttonState = 0;  // 0 for button released, 1 for button pressed
-    int prevButtonState = 0;
-    unsigned long buttonPressStartTime = 0;
-
-    while (1) {
-        buttonState = PIND & (1 << BUTTON_PIN);
-
-        if (buttonState == 0 && prevButtonState == 1) {
-            // Button released after a long press (2 seconds)
-            unsigned long buttonPressDuration = millis() - buttonPressStartTime;
-            if (buttonPressDuration >= 2000) {
-                changeEffect();  // Change the effect
+                PORTD = ~data;
+                if (direction == 0) {
+                    data <<= 1;
+                } else {
+                    data >>= 1;
+                }
+                if (data == 0) {
+                    if (direction == 0) {
+                        data = 1;
+                    } else {
+                        data = 128;
+                    }
+                }
             }
-        } else if (buttonState == 1 && prevButtonState == 0) {
-            // Button pressed
-            buttonPressStartTime = millis();
         }
-
-        // Run the selected effect
-        if (currentEffect == 0) {
-            runningLight();
-        } else {
-            shadow();
-        }
-
-        prevButtonState = buttonState;
+        // 10 ms thread ---------
+        scan_key();
+        _delay_ms(10);
     }
-
     return 0;
 }
