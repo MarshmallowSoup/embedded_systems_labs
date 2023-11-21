50ddd8e0c4f576326012b32de55b7d9ce8db09e0
Volodymyr Lysetskyi <vlysetskyi@dedicatted.com>
2023-11-21 21:14:22 +0200

    Refactored lab 2

--- lab2/main.c
+++ lab2/main.c
@@ -1,11 +1,11 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define SECOND_BUTTON PB2
-
 uint8_t direction = 0, data = 1, is_run = 1, double_click_count = 0;
+uint8_t effect_index = 0;
 void key_handler(uint8_t ks);
 uint8_t second_button_state = 0;
+uint8_t speed = 10;
 
 void scan_key(void)
 {
@@ -13,21 +13,21 @@ void scan_key(void)
     static uint8_t long_press_cnt = 0;
 
     shreg <<= 1;
-    if((PINB & (1 << PB1)) != 0) {
+    if ((PINC & (1 << PC5)) != 0) {
         shreg |= 1;
     }
     // push key
-    if((shreg & 0x07) == 0x04) {
+    if ((shreg & 0x07) == 0x04) {
         long_press_cnt = 0;
         key_handler(0);
     }
     // up key
-    if((shreg & 0x0F) == 0x03) {
+    if ((shreg & 0x0F) == 0x03) {
         if(long_press_cnt < 100){
-        key_handler(1);
+            key_handler(1);
         }
     }
-    if(shreg == 0){
+    if (shreg == 0){
         if(long_press_cnt < 254){
             long_press_cnt++;
         }
@@ -35,57 +35,14 @@ void scan_key(void)
             key_handler(2); //long press
         } 
     }
-
-    shreg <<= 1;
-    if ((PINB & (1 << SECOND_BUTTON)) != 0) {
-        shreg |= 1;
-    }
-
-    // Long press for the second button
-    if (shreg == 0x80) {
-        key_handler(3); // Long press for the second button
-    }
 }
 
-
-void running_lights(uint8_t* direction, uint8_t* data)
-{
-    static uint8_t dl = 0;
-    static uint8_t local_direction = 0;
-    static uint8_t local_data = 1;
-
-    if (is_run) {
-        if (++dl > 5) {
-            dl = 0;
-            PORTD = ~local_data;
-
-            if (local_direction == 0) {
-                local_data <<= 1;
-            } else {
-                local_data >>= 1;
-            }
-
-            if (local_data == 0) {
-                if (local_direction == 0) {
-                    local_data = 1;
-                } else {
-                    local_data = 128;
-                }
-            }
-        }
-
-        *direction = local_direction; // Update the direction variable
-        *data = local_data;           // Update the data variable
-    }
-}
-
-
-void opposite_edges_animation(void)
+void opposite_edges_animation()
 {
     // New animation: LEDs turn on and off from two edges in the opposite order
     static uint8_t dl = 0;
     if (is_run) {
-        if (++dl > 5) {
+        if (++dl > speed) {
             dl = 0;
             if (direction == 0) {
                 PORTD = data;
@@ -106,46 +63,134 @@ void opposite_edges_animation(void)
     }
 }
 
-void key_management(void)
+
+void light_up_one_by_one()
 {
-    // This function is called in the main loop to manage keys
-    scan_key(); // Call the existing scan_key function
+    static uint8_t dl = 0;
+    static uint8_t local_direction = 0;
+    static uint8_t local_data = 1;
+
+    if (is_run) {
+        if (++dl > speed) { // Delay for 1 second (50 * 20ms)
+            dl = 0;
+
+            PORTD = ~local_data;
+
+            if (local_direction == 0) {
+                local_data <<= 1;
+            } else {
+                local_data >>= 1;
+            }
+
+            if (local_data == 0) {
+                if (local_direction == 0) {
+                    local_data = 1;
+                } else {
+                    local_data = 128;
+                }
+            }
+        }
+    }
+}
+
+void running_lights()
+{
+    static uint8_t dl = 0;
+    static uint8_t local_direction = 0;
+    static uint8_t local_data = 1;
+
+    if (is_run) {
+        if (++dl > speed) {
+            dl = 0;
+            PORTD = ~local_data;
+
+            if (local_direction == 0) {
+                local_data <<= 1;
+            } else {
+                local_data >>= 1;
+            }
+
+            if (local_data == 0) {
+                if (local_direction == 0) {
+                    local_data = 1;
+                } else {
+                    local_data = 128;
+                }
+            }
+        }
+    }
+}
+
+
+void even_odd_light_sequence()
+{
+    static uint8_t dl = 0;
+    static uint8_t local_direction = 0;
+    static uint8_t local_data = 0b01010100; 
+    if (is_run) {
+        if (++dl > 10) {
+            dl = 0;
+
+            PORTD = ~local_data;
+
+            if (local_direction == 0) {
+                local_data <<= 1;
+                if (local_data == 0) {
+                    local_data = 0b10101000; 
+                    local_direction = 1; 
+                }
+            } else {
+                local_data <<= 1;
+                if (local_data == 0) {
+                    local_data = 0b01010100;
+                    local_direction = 0; 
+                }
+            }
+        }
+    }
 }
 //-----------------------------------------------------------
 void key_handler(uint8_t ks)
 {
     if (ks == 1) { // Short key up for the first button
-        is_run = (is_run == 0) ? 1 : 0;
-        running_lights(0, 1);
+        effect_index++;
+        if(effect_index >3){
+            effect_index = 0;
+        }
     } else if (ks == 2) {  // Long key press for the first button
-        direction ^= 1;
-        is_run = 1;
-        running_lights(0, 1);
-    } else if (ks == 3) { // Long key press for the second button
-        opposite_edges_animation(); // Call the new animation function
-    }
+        speed += 10;
+        if (speed > 50){
+            speed = 10;
+        }
+    }   
 }
 
 //-----------------------------------------------------------
 int main(void)
-{
-    uint8_t dl = 0;
-    // Налаштування портів вводу-виводу
-    DDRD = 0xFF; // Всі лінії порта D виходи
-    DDRB = 0; // Всі лінії порта B входи
+{ 
+    DDRC = 0;
+    DDRD = 255;
     UCSR0B = 0;
-    // Initialization code
 
-   
     for (;;) {
-        // LED animation thread
-        running_lights(0, 1);
-
-        // Key management thread
-        key_management();
-
-        // Delay for 10 ms
+        scan_key();
         _delay_ms(10);
+        switch (effect_index) {
+            case 0:
+                opposite_edges_animation();
+                break;
+            case 1:
+                light_up_one_by_one();
+                break;
+            case 2:
+                running_lights();
+                break;
+            case 3:
+                even_odd_light_sequence();
+                break;
+            default:
+                break;
+        }
     }
     return 0;
 }
